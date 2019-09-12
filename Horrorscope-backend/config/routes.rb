e1d0a6cf9808adce1801dff05715d287f9ac49c5
Dan-Vy Le <github email address>
2019-09-12 09:24:37 -0700

    scheduling tasks

--- Horrorscope-backend/config/routes.rb
+++ Horrorscope-backend/config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   resources :users
   get '/fetch_moon', to: 'retrogrades#fetch_moon'
   get '/fetch_books', to: 'retrogrades#fetch_books'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
